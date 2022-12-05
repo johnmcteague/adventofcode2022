e156d6543c8c801ba138c77c0c4a7e827b6c8a30
John McTeague <john@mcteague.co.uk>
2022-12-05 22:50:12 +0000

    Cleanup

--- day3/answer_a.py
+++ day3/answer_a.py
@@ -1,9 +1,9 @@
-
 TOTAL = 0
 
 LOOKUP = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def score(shared):
+def score(shared: list) -> int:
+    ''' Handle scenario where there is more than 1 shared item'''
     total = 0
     for elem in shared:
         total = LOOKUP.index(elem) + 1
@@ -12,15 +12,12 @@ def score(shared):
 with open('input', 'r', encoding='utf-8') as f:
     lines = f.readlines()
     for line in lines:
-        length_of_input = len(line)
-        num_items = int(length_of_input/2)
+        num_items_per_rucksack = int(len(line)/2)
 
-        compartment_1 = line[0:num_items]
-        compartment_2 = line[num_items:]
-        shared = [value for value in compartment_1 if value in compartment_2]
-        unique_shared = list(dict.fromkeys(shared))
+        compartment_1 = line[0:num_items_per_rucksack] # first half
+        compartment_2 = line[num_items_per_rucksack:] # second half
+        shared = [value for value in compartment_1 if value in compartment_2] # intersection of two lists
+        unique_shared = list(dict.fromkeys(shared)) # quick way to remove duplicates
         TOTAL = TOTAL + score(unique_shared)
-
-        print(f'{line} - {compartment_1} - {compartment_2} - {unique_shared}')
 
 print(TOTAL)
