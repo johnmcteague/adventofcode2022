569800244139ecaf0b5304a5340be2abca4b82ce
John McTeague <john@mcteague.co.uk>
2022-12-06 23:19:48 +0000

    Day 6 complete

--- day5/answer.py
+++ day5/answer.py
@@ -1,15 +1,65 @@
+import re
 
-with open('example', 'r') as f:
-    lines = f.readlines()
+CARGO_REGEX = '[{A-Z}]'
 
-grid_definition = []
-# load the starting grid
-for line in lines:
-    if line == '\n':
-        break
-    grid_definition.append(line)
+class Move:
+    def __init__(self, source, target, count, model=9000):
+        self.source = source
+        self.target = target
+        self.count = count
+        self.model = model
 
-number_stacks = grid_definition[len(grid_definition) - 1].replace('   ',',')
-print(number_stacks)
 
-#execute instructions
+    def apply(self, crates):
+        #self.debug_creates(crates)
+
+        if self.model == 9000:
+            for i in range (0, self.count):
+                to_move = crates[self.source-1].pop()
+                crates[self.target-1].append(to_move)
+        elif self.model == 9001:
+            to_move = []
+            for i in range (0, self.count):
+                to_move.append(crates[self.source-1].pop())
+            to_move.reverse()
+            crates[self.target-1].extend(to_move)
+        else:
+            raise ValueError("Invalid model")
+
+    def debug_creates(self, crates):
+        for stack in crates:
+            print(stack, end=',')
+        print('')
+
+def load_data(file='input'):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+def load_crates(data):
+    crates = []
+    num_crates = int((len(data[0]))/4)
+    for i in range(0, num_crates):
+        crates.append([])
+
+    for line in data:
+        matches = re.finditer(CARGO_REGEX, line)
+        for m in matches:
+            #print(m.start(), m.end(), m.group(0), m )
+            bin = int(m.start()/4)
+            crates[bin].insert(0,m.group())
+        if line == '\n':
+            break
+    return crates
+
+def process_moves(crates, data, model):
+    for line in data:
+        if line.startswith('move'):
+            move = line.split(' ') # should probably be able to regex this
+            Move(int(move[3]), int(move[5]), int(move[1]), model).apply(crates) 
+
+data = load_data()
+crates = load_crates(data)
+process_moves(crates, data, 9001)
+for stack in crates:
+    print(stack[len(stack) - 1], end='')
